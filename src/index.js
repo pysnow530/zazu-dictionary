bd9924e432a4c5bd69d5af5bfcf3a47b01939a09
pysnow530 <pysnow530@163.com>
2023-09-07 21:59:58 +0800

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-const got = require('got')
+const https = require('https')
 
 module.exports = (pluginContext) => {
   return (word, env = {}) => {
     return new Promise((resolve, reject) => {
-      got(`https://dict.youdao.com/suggest?num=5&ver=3.0&doctype=json&cache=false&le=en&q=${word}`, {json: true})
-	.then(res => res.body.data.entries.map(entry => resolve([
-          {
-            icon: 'fa-book',
-            title: entry.entry,
-            subtitle: entry.explain,
-          },
-	])))
+      const url = `https://dict.youdao.com/suggest?num=5&ver=3.0&doctype=json&cache=false&le=en&q=${word}`
+      pluginContext.console.log('info', 'Request to youdao', {url})
+
+      https.get(url, (response) => {
+        let responseText = ''
+
+        response.on('data', (chunk) => {
+          responseText += chunk
+        })
+
+        response.on('end', () => {
+          pluginContext.console.log('info', 'Response from youdao', {responseText})
+          resolve(JSON.parse(responseText).data.entries.map(entry => ({
+            icon: 'fa-book', title: entry.entry, subtitle: entry.explain, value: entry.entry,
+          })))
+        })
+      }).on('error', (error) => {
+        pluginContext.console.log('Request to dict.youdao.com error', {error})
+      })
     })
   }
 }
