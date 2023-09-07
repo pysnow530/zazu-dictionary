e07962b4fe423a7e0408a3e843172cfe7bd410bb
pysnow530 <pysnow530@163.com>
2023-09-07 21:29:14 +0800

    Fix fetch()

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+const got = require('got')
+
 module.exports = (pluginContext) => {
   return (word, env = {}) => {
     return new Promise((resolve, reject) => {
-      fetch(`https://dict.youdao.com/suggest?num=5&ver=3.0&doctype=json&cache=false&le=en&q=${word}`)
-        .then(rsp => rsp.json())
-        .then(data => data.data.entries)
-	.then(entries.map(entry => resolve([
+      got(`https://dict.youdao.com/suggest?num=5&ver=3.0&doctype=json&cache=false&le=en&q=${word}`, {json: true})
+	.then(res => res.body.data.entries.map(entry => resolve([
           {
             icon: 'fa-book',
             title: entry.entry,
